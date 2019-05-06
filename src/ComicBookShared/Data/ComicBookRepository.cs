1fbcb708e00653cfb85eb786bb3aec3a359d4c4c
hkoflax <hkoflax@users.noreply.github.com>
2019-05-06 00:56:15 -0400

    Initial Comic book Web App

---
+++ src/ComicBookShared/Data/ComicBookRepository.cs
@@ -0,0 +1,53 @@
+﻿using ComicBookShared.Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookShared.Data
+{
+    public class ComicBookRepository : BaseRepository<ComicBook>
+    {
+        public ComicBookRepository(Context context) : base (context)
+        {
+        }
+        public override IList<ComicBook> GetList()
+        {
+            return Context.ComicBooks
+                    .Include(cb => cb.Series)
+                    .OrderBy(cb => cb.Series.Title)
+                    .ThenBy(cb => cb.IssueNumber)
+                    .ToList();
+        }
+        public override ComicBook Get(int id, bool includeRelatedEntities = true)
+        {
+            var comicBooks = Context.ComicBooks.AsQueryable();
+            if (includeRelatedEntities)
+            {
+                comicBooks = comicBooks
+                    .Include(cb => cb.Series)
+                    .Include(cb => cb.Artists.Select(a => a.Artist))
+                    .Include(cb => cb.Artists.Select(a => a.Role));
+            }
+            return comicBooks
+                    .Where(cb => cb.Id == id)
+                    .SingleOrDefault();
+        }
+        public bool ComicBookSerieshasissueNumber(int comicBookId, int seriesId, int issueNumber)
+        {
+            return Context.ComicBooks
+                    .Any(cb => cb.Id != comicBookId &&
+                               cb.SeriesId == seriesId &&
+                               cb.IssueNumber == issueNumber);
+        }
+        public bool ComicBookhasArtistRoleCombination(int comicBookId, int artistId, int roleId)
+        {
+            return Context.ComicBookArtists
+                    .Any(cba => cba.ComicBookId == comicBookId &&
+                                cba.ArtistId == artistId &&
+                                cba.RoleId == roleId);
+        }
+    }
+}
